525542f54f1f6569631b89fadfbc91ed5a7f74de
James Ononiwu <jamesononiwu@gmail.com>
2023-03-08 09:46:16 -0800

    added tests for FileStorage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,129 @@
+
+#!/usr/bin/python3
+"""
+Unit Test for BaseModel Class
+"""
+import unittest
+from datetime import datetime
+import models
+import json
+import os
+
+BaseModel = models.base_model.BaseModel
+FileStorage = models.engine.file_storage.FileStorage
+storage = models.storage
+F = 'file.json'
+
+
+class TestFileStorageDocs(unittest.TestCase):
+    """Class for testing BaseModel docs"""
+
+    @classmethod
+    def setUpClass(cls):
+        """Set up testing """
+        print('\n\n.................................')
+        print('..... Testing Documentation .....')
+        print('..... For FileStorage Class .....')
+        print('.................................\n')
+
+
+    def test_checking_for_docstring_BaseModel(self):
+        """checking for docstrings"""
+        self.assertIsNotNone(FileStorage.__doc__)
+        self.assertIsNotNone(FileStorage.all.__doc__)
+        self.assertIsNotNone(FileStorage.new.__doc__)
+        self.assertIsNotNone(FileStorage.save.__doc__)
+        self.assertIsNotNone(FileStorage.reload.__doc__)
+
+
+
+class TestBmFsInstances(unittest.TestCase):
+    """testing for class instances"""
+
+    @classmethod
+    def setUpClass(cls):
+        print('\n\n.................................')
+        print('...... Testing FileStorate ......')
+        print('..... For FileStorage Class .....')
+        print('.................................\n\n')
+
+    def setUp(self):
+        """initializes new storage object for testing"""
+        self.storage = FileStorage()
+        self.bm_obj = BaseModel()
+
+    def test_instantiation(self):
+        """... checks proper FileStorage instantiation"""
+        self.assertIsInstance(self.storage, FileStorage)
+
+    def test_storage_file_exists(self):
+        """... checks proper FileStorage instantiation"""
+        os.remove(F)
+        self.bm_obj.save()
+        self.assertTrue(os.path.isfile(F))
+
+    def test_all(self):
+        """... checks if all() function returns newly created instance"""
+        bm_id = self.bm_obj.id
+        all_obj = storage.all()
+        actual = 0
+        for k in all_obj.keys():
+            if bm_id in k:
+                actual = 1
+        self.assertTrue(1 == actual)
+
+    def test_obj_saved_to_file(self):
+        """... checks proper FileStorage instantiation"""
+        os.remove(F)
+        self.bm_obj.save()
+        bm_id = self.bm_obj.id
+        actual = 0
+        with open(F, mode='r', encoding='utf-8') as f_obj:
+            storage_dict = json.load(f_obj)
+        for k in storage_dict.keys():
+            if bm_id in k:
+                actual = 1
+        self.assertTrue(1 == actual)
+
+    def test_to_dict(self):
+        """... to_dict should return serializable dict object"""
+        my_model_dict = self.bm_obj.to_dict()
+        actual = 1
+        try:
+            serialized = json.dumps(my_model_dict)
+        except:
+            actual = 0
+        self.assertTrue(1 == actual)
+
+    def test_reload(self):
+        """... checks proper usage of reload function"""
+        os.remove(F)
+        self.bm_obj.save()
+        bm_id = self.bm_obj.id
+        actual = 0
+        new_storage = FileStorage()
+        new_storage.reload()
+        all_obj = new_storage.all()
+        for k in all_obj.keys():
+            if bm_id in k:
+                actual = 1
+        self.assertTrue(1 == actual)
+
+    def test_save_reload_class(self):
+        """... checks proper usage of class attribute in file storage"""
+        os.remove(F)
+        self.bm_obj.save()
+        bm_id = self.bm_obj.id
+        actual = 0
+        new_storage = FileStorage()
+        new_storage.reload()
+        all_obj = new_storage.all()
+        for k, v in all_obj.items():
+            if bm_id in k:
+                if type(v).__name__ == 'BaseModel':
+                    actual = 1
+        self.assertTrue(1 == actual)
+
+
+if __name__ == '__main__':
+    unittest.main
