f22c1c8fc9fee8b7b825e657c33580a2a76397af
James Ononiwu <jamesononiwu@gmail.com>
2023-03-07 17:09:39 -0800

    added args and kwargs to base_model init

--- models/base_model.py
+++ models/base_model.py
@@ -10,14 +10,25 @@ time_format = "%Y-%m-%dT%H:M:%S.%f"
 class BaseModel:
     """Base class for other classes
     """
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """Initialize BaseModel class
         id: string - assigned with an uuis when an instance is created
         created_at: datetime - assign with the current datetime
         updated_at: datetime - update timestamp everytime object changes
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
+
+        if kwargs:
+            self.__dict__ = kwargs
+            if "created_at" in kwargs:
+                self.created_at = datetime.strptime(kwargs.get("created_at"),
+                                                    time_format)
+            if "updated_at" in kwargs:
+                self.updated_At = datetime.strptime(kwargs.get("updated_at"),
+                                                    time_format)
+
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
 
 
     def save(self):
