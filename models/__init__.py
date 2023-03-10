8d2fe5635811d199ab750322b3102c05f09bbfd2
James Ononiwu <jamesononiwu@gmail.com>
2023-03-10 08:27:44 -0800

    added and tested classes User, State, City, Amenity, Place, Review

--- models/__init__.py
+++ models/__init__.py
@@ -3,11 +3,20 @@
 """
 from models.base_model import BaseModel
 from models.user import User
+from models.place import Place
+from models.state import State
+from models.city import City
+from models.amenity import Amenity
+from models.review import Review
 from models.engine.file_storage import FileStorage
 storage = FileStorage()
 storage.reload()
 
 classes = {
         "BaseModel": BaseModel,
-        "User": User
+        "User": User,
+        "State": State,
+        "City": City,
+        "Amenity": Amenity,
+        "Review": Review
         }
